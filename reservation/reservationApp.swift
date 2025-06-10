cda2478708f1b2861e64fa4ac99f0f762be4f00b
U-TED <meganenasi@gmail.com>
2025-06-11 07:23:45 +0900

    Initial Commit

---
+++ reservation/reservationApp.swift
@@ -0,0 +1,17 @@
+//
+//  reservationApp.swift
+//  reservation
+//
+//  Created by 飯田優斗 on 2025/06/11.
+//
+
+import SwiftUI
+
+@main
+struct reservationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ reservationTests/reservationTests.swift
@@ -0,0 +1,17 @@
+//
+//  reservationTests.swift
+//  reservationTests
+//
+//  Created by 飯田優斗 on 2025/06/11.
+//
+
+import Testing
+@testable import reservation
+
+struct reservationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
